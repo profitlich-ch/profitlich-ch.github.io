712d7440161e714d97e710aa360f634fb1d362b8
Moritz Profitlich <m.profitlich@profitlich.ch>
2025-11-11 10:14:30 +0100

    Kommentarsystem Giscus integriert

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,10 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+import { toRefs } from "vue";
+
 export default {
     extends: DefaultTheme,
     Layout: () => {
@@ -11,7 +15,27 @@ export default {
             // https://vitepress.dev/guide/extending-default-theme#layout-slots
         })
     },
-    enhanceApp({ app, router, siteData }) {
-        // ...
-    }
+    // enhanceApp({ app, router, siteData }) {
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+    },
+    setup() {
+        const { frontmatter } = toRefs(useData());
+        const route = useRoute();
+
+        giscusTalk({
+            repo: 'profitlich-ch/profitlich-ch.github.io',
+            repoId: 'R_kgDOP2rVgQ',
+            category: 'Handbuchkommentare',
+            categoryId: 'DIC_kwDOP2rVgc4Cxq74',
+            mapping: 'pathname',
+            inputPosition: 'top',
+            lang: 'de',
+            theme: 'preferred_color_scheme',
+            loading: 'lazy',
+        }, {
+            frontmatter,
+            route
+        }, true);
+    },
 } satisfies Theme
